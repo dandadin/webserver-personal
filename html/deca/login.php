d7946a4642a7ed1f632f6d956f0184fcb68655aa
Dan Simanek <dan.simanek@seznam.cz>
2023-02-20 17:31:53 +0100

    Switched repositories

--- html/deca/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-    if (isset($_POST["heslo"])) {
-        if ($_POST["heslo"] == "1189") include "deca.html";
-        else include "error.html";
-    } else header("Location: /deca");
--- startup-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/deca/init;
-docker-compose up -d & sleep 10;
-docker-compose down & sleep 5;
-cd ..;
-docker-compose up -d;
