2f7b6389c5a0fb6d7eb63c5b31cf733113313f00
dandadin <72394732+dandadin@users.noreply.github.com>
2023-03-11 22:40:54 +0100

    index.php

---
+++ html/index.php
@@ -0,0 +1,9 @@
+<hmtl>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+    <h1>Mapa</h1>
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1s7garO3XJjzSpBNm99ooMalfS40Anpo&ehbc=2E312F" width="640" height="480"></iframe>
+  </body>
+</html>
